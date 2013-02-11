1e6cb09add6085b65f6d36a7fc2968380330b459
Matt Latham <mlatham@Matt-Lathams-MacBook-Pro-2.local>
2013-02-11 01:20:15 -0500

    Tweak AFView to match IB defaults

--- AFToolkit/MVC/AFView.m
+++ AFToolkit/MVC/AFView.m
@@ -5,6 +5,19 @@
 
 #pragma mark Constants
 
+#define IB_DEFAULT_AUTORESIZINGMASK (UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth)
+#define IB_DEFAULT_CONTENTMODE UIViewContentModeScaleToFill
+#define IB_DEFAULT_TAG 0
+#define IB_DEFAULT_USERINTERACTIONENABLED YES
+#define IB_DEFAULT_MULTIPLETOUCHENABLED YES
+#define IB_DEFAULT_ALPHA 1.f
+#define IB_DEFAULT_BACKGROUNDCOLOR [UIColor whiteColor]
+#define IB_DEFAULT_OPAQUE YES
+#define IB_DEFAULT_HIDDEN NO
+#define IB_DEFAULT_CLEARSCONTEXTBEFOREDRAWING YES
+#define IB_DEFAULT_CLIPSTOBOUNDS NO
+#define IB_DEFAULT_AUTORESIZESSUBVIEWS YES
+
 
 #pragma mark - Class Extensions
 
@@ -41,6 +54,7 @@
 
 - (id)initWithUniversalNibName: (NSString *)nibName
 {
+	// Load the composite view.
 	if ((self = [super initWithFrame: CGRectZero]) == nil)
 	{
 		return nil;
@@ -49,7 +63,7 @@
 	// Load the composite view.
 	[self AF_loadViewWithUniversalNibName: nibName 
 		bundle: nil];
-	
+		
 	return self;
 }
 
@@ -120,20 +134,87 @@
 	// Get the decoded view.
 	UIView *view = [nibContents objectAtIndex: 0];
 
-	// Add the view as a subview of this view.
-	[self addSubview: view];
+	// Transfer the auto-resizing mask, if it's not the Interface Builder default.
+	if (self.autoresizingMask == IB_DEFAULT_AUTORESIZINGMASK)
+	{
+		self.autoresizingMask = view.autoresizingMask;
+	}
 	
-	// Ensure this container view has a clear background color.
-	self.backgroundColor = [UIColor clearColor];
+	// Transfer the content mode, if it's not the Interface Builder default.
+	if (self.contentMode == IB_DEFAULT_CONTENTMODE)
+	{
+		self.contentMode = view.contentMode;
+	}
 	
-	// Align the view's autoresize mask with this view.
-	self.autoresizingMask = view.autoresizingMask;
+	// Transfer the tag, if it's not the Interface Builder default.
+	if (self.tag == IB_DEFAULT_TAG)
+	{
+		self.tag = view.tag;
+	}
 	
-	// Always transfer this view's width and height values to the subview.
+	// Transfer the user-interaction enabled flag, if it's not the Interface Builder default.
+	if (self.userInteractionEnabled == IB_DEFAULT_USERINTERACTIONENABLED)
+	{
+		self.userInteractionEnabled = view.userInteractionEnabled;
+	}
+	
+	// Transfer the multiple touch enabled flag, if it's not the Interface Builder default.
+	if (self.multipleTouchEnabled == IB_DEFAULT_MULTIPLETOUCHENABLED)
+	{
+		self.multipleTouchEnabled = view.multipleTouchEnabled;
+	}
+	
+	// Transfer the alpha, if it's not the Interface Builder default.
+	if (self.alpha == IB_DEFAULT_ALPHA)
+	{
+		self.alpha = view.alpha;
+	}
+	
+	// Transfer the opaque value, if it's not the Interface Builder default.
+	if (self.opaque == IB_DEFAULT_OPAQUE)
+	{
+		self.opaque = view.opaque;
+	}
+	
+	// Transfer the hidden value, if it's not the Interface Builder default.
+	if (self.hidden == IB_DEFAULT_HIDDEN)
+	{
+		self.hidden = view.hidden;
+	}
+	
+	// Transfer the clears graphics context value, if it's not the Interface Builder default.
+	if (self.clearsContextBeforeDrawing == IB_DEFAULT_CLEARSCONTEXTBEFOREDRAWING)
+	{
+		self.clearsContextBeforeDrawing = view.clearsContextBeforeDrawing;
+	}
+	
+	// Transfer the clips subviews value, if it's not the Interface Builder default.
+	if (self.clipsToBounds == IB_DEFAULT_CLIPSTOBOUNDS)
+	{
+		self.clipsToBounds = view.clipsToBounds;
+	}
+	
+	// Transfer the autoresizes subviews value, if it's not the Interface Builder default.
+	if (self.autoresizesSubviews == IB_DEFAULT_AUTORESIZESSUBVIEWS)
+	{
+		self.autoresizesSubviews = view.autoresizesSubviews;
+	}
+	
+	// Always transfer the view's background color.
+	self.backgroundColor = view.backgroundColor;
+	
+	// Always transfer the view's width and height values.
 	CGRect frame = self.frame;
 	frame.size.width = view.frame.size.width;
 	frame.size.height = view.frame.size.height;
 	self.frame = frame;
+	
+	// Transfer all subviews of the decoded view to the target view.
+	for (UIView *subview in view.subviews)
+	{
+		[subview removeFromSuperview];
+		[self addSubview: subview];
+	}
 }
 
 
