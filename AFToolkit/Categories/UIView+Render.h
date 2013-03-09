12c5a7c8c312a9b856bf5f6a49246af4f7f30cc0
Matt Latham <mlatham@Matt-Lathams-MacBook-Pro-2.local>
2013-03-09 15:33:12 -0500

    Add render category on UIView.

---
+++ AFToolkit/Categories/UIView+Render.h
@@ -0,0 +1,13 @@
+
+
+#pragma mark Class Interface
+
+@interface UIView (Render)
+
+
+#pragma mark - Instance Methods
+
+- (UIImage *)render;
+
+
+@end // @interface UIView (Render)
---
+++ AFToolkit/Categories/UIView+Render.m
@@ -0,0 +1,39 @@
+#import "UIView+Render.h"
+#import <QuartzCore/QuartzCore.h>
+
+
+#pragma mark Class Definition
+
+@implementation UIView (Render)
+
+
+#pragma mark - Properties
+
+
+#pragma mark - Public Methods
+
+- (UIImage *)render
+{
+	// Begin drawing. Note that passing in a scale of 0.0 uses the current view's scale.
+	UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.opaque, 0.0);
+ 
+	// Clear the context to black.
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	[[UIColor blackColor] set];
+	CGContextFillRect(context, self.bounds);
+ 
+	// Render in the view's layer.
+	[self.layer renderInContext: context];
+ 
+	// Get rendered layer image.
+	UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+
+	// End drawing.
+	UIGraphicsEndImageContext();
+	
+	// Return the rendered image.
+	return image;
+}
+
+
+@end // @implementation UIView (Render)
