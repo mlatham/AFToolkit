c23bbc3a79147869e5436a67341e38f410df8601
Matt Latham <matt.e.latham@gmail.com>
2013-04-11 23:39:46 -0400

    Don't transfer properties from nib view.

--- AFToolkit/MVC/AFView.m
+++ AFToolkit/MVC/AFView.m
@@ -133,88 +133,15 @@
 
 	// Get the decoded view.
 	UIView *view = [nibContents objectAtIndex: 0];
-
-	// Transfer the auto-resizing mask, if it's not the Interface Builder default.
-	if (self.autoresizingMask == IB_DEFAULT_AUTORESIZINGMASK)
-	{
-		self.autoresizingMask = view.autoresizingMask;
-	}
-	
-	// Transfer the content mode, if it's not the Interface Builder default.
-	if (self.contentMode == IB_DEFAULT_CONTENTMODE)
-	{
-		self.contentMode = view.contentMode;
-	}
-	
-	// Transfer the tag, if it's not the Interface Builder default.
-	if (self.tag == IB_DEFAULT_TAG)
-	{
-		self.tag = view.tag;
-	}
-	
-	// Transfer the user-interaction enabled flag, if it's not the Interface Builder default.
-	if (self.userInteractionEnabled == IB_DEFAULT_USERINTERACTIONENABLED)
-	{
-		self.userInteractionEnabled = view.userInteractionEnabled;
-	}
-	
-	// Transfer the multiple touch enabled flag, if it's not the Interface Builder default.
-	if (self.multipleTouchEnabled == IB_DEFAULT_MULTIPLETOUCHENABLED)
-	{
-		self.multipleTouchEnabled = view.multipleTouchEnabled;
-	}
-	
-	// Transfer the alpha, if it's not the Interface Builder default.
-	if (self.alpha == IB_DEFAULT_ALPHA)
-	{
-		self.alpha = view.alpha;
-	}
-	
-	// Transfer the opaque value, if it's not the Interface Builder default.
-	if (self.opaque == IB_DEFAULT_OPAQUE)
-	{
-		self.opaque = view.opaque;
-	}
-	
-	// Transfer the hidden value, if it's not the Interface Builder default.
-	if (self.hidden == IB_DEFAULT_HIDDEN)
-	{
-		self.hidden = view.hidden;
-	}
-	
-	// Transfer the clears graphics context value, if it's not the Interface Builder default.
-	if (self.clearsContextBeforeDrawing == IB_DEFAULT_CLEARSCONTEXTBEFOREDRAWING)
-	{
-		self.clearsContextBeforeDrawing = view.clearsContextBeforeDrawing;
-	}
-	
-	// Transfer the clips subviews value, if it's not the Interface Builder default.
-	if (self.clipsToBounds == IB_DEFAULT_CLIPSTOBOUNDS)
-	{
-		self.clipsToBounds = view.clipsToBounds;
-	}
-	
-	// Transfer the autoresizes subviews value, if it's not the Interface Builder default.
-	if (self.autoresizesSubviews == IB_DEFAULT_AUTORESIZESSUBVIEWS)
-	{
-		self.autoresizesSubviews = view.autoresizesSubviews;
-	}
 	
 	// Always transfer the view's background color.
 	self.backgroundColor = view.backgroundColor;
 	
-	// Always transfer the view's width and height values.
-	CGRect frame = self.frame;
-	frame.size.width = view.frame.size.width;
-	frame.size.height = view.frame.size.height;
-	self.frame = frame;
+	// Align the nib view's frame with the container view's frame.
+	view.frame = self.bounds;
 	
-	// Transfer all subviews of the decoded view to the target view.
-	for (UIView *subview in view.subviews)
-	{
-		[subview removeFromSuperview];
-		[self addSubview: subview];
-	}
+	// Add the nib view as a child of this view.
+	[self addSubview: view];
 }
 
 
